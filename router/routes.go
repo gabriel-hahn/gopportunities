d4c5d18bfce0757ec50c769471b4219095e0a92b
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-06-29 19:20:08 -0300

    chore: add initial swagger setup

--- router/routes.go
+++ router/routes.go
@@ -1,14 +1,20 @@
 package router
 
 import (
+	docs "github.com/gabriel-hahn/gopportunities/docs"
 	"github.com/gabriel-hahn/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
 
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -16,4 +22,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
